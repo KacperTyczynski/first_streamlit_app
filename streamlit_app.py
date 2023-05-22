d2609b312f7d5c1dab21aceb8de16e2e5eba6b2d
KacperTyczynski <43890890+KacperTyczynski@users.noreply.github.com>
2023-05-22 15:59:27 +0200

    Update streamlit_app.py
    Checking Python syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie', \n, 'Hard Boiled Free-Range Egg' )
+streamlit.text("""Kale, Spinach & Rocket Smoothie
+                  Hard Boiled Free-Range Egg""" )
