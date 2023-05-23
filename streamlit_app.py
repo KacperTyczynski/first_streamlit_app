4db5be29efa49767c35a8071b353cac6e707c134
KacperTyczynski <43890890+KacperTyczynski@users.noreply.github.com>
2023-05-23 14:15:18 +0200

    Update streamlit_app.py
    Removed unnecessary rows

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,6 @@ streamlit.dataframe(fruits_to_show)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
-fruityvice_responde = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-
 streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
